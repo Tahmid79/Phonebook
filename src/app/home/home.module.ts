c119bf238af6140472ddfbc6577c358e629a61d8
Tahmid <tahmidfarhan12@gmail.com>
2022-09-09 14:25:19 +0600

    init

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeMainComponent } from './components/home-main/home-main.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    HomeMainComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule
+  ]
+})
+export class HomeModule { }
