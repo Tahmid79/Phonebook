685cc788e8b787ee75c48684cb32b4c1617954a3
Tahmid <tahmidfarhan12@gmail.com>
2022-09-09 22:06:28 +0600

    all alphabets

--- src/app/home/components/home-main/home-main.component.ts
+++ src/app/home/components/home-main/home-main.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-main.component.scss']
 })
 export class HomeMainComponent implements OnInit {
-
+  alphabets: string[] = [] ;
   constructor() { }
 
   ngOnInit(): void {
+    const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+    this.alphabets = alpha.map((x) => String.fromCharCode(x));
   }
 
 }
