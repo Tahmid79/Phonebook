91f36f1bb8e8fd6ea10d260cd8a24b567db97978
Tahmid <tahmidfarhan12@gmail.com>
2022-09-12 00:39:02 +0600

    show phone numbers

--- src/app/home/components/contact-details/contact-details.component.ts
+++ src/app/home/components/contact-details/contact-details.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent implements OnInit {
-
+  numbers: string[] = ['+88 017 XXXXXXX' , '+60 011 XXXXXXX'] ;
+  phone1 = '+88 017 XXXXXXX' ;
+  phone2 = '+60 011 XXXXXXX' ;
   constructor() { }
 
   ngOnInit(): void {
