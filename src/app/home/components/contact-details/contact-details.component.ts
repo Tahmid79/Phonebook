572f4e3a1432c38e0e4fcda5d3099752705cb034
Tahmid <tahmidfarhan12@gmail.com>
2022-09-12 01:14:51 +0600

    info containers

--- src/app/home/components/contact-details/contact-details.component.ts
+++ src/app/home/components/contact-details/contact-details.component.ts
@@ -9,6 +9,14 @@ export class ContactDetailsComponent implements OnInit {
   numbers: string[] = ['+88 017 XXXXXXX' , '+60 011 XXXXXXX'] ;
   phone1 = '+88 017 XXXXXXX' ;
   phone2 = '+60 011 XXXXXXX' ;
+  email = 'baybaythegoat@gmail.com' ;
+
+  information = [
+    {infoType: 'Email' , infoValue : 'baybaythegoat@gmail.com'},
+    {infoType: 'Profile' , infoValue : 'www.linkedin.com/237494505/baybay00'},
+    {infoType: 'Address' , infoValue : 'House no. somewhere, Road no. 23'},
+  ] ;
+
   constructor() { }
 
   ngOnInit(): void {
