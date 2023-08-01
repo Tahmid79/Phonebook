305cc3247069fcbbd24bdb841386cbff849407a4
Tahmid <tahmidfarhan12@gmail.com>
2023-08-02 02:33:33 +0600

    add contact details model

---
+++ src/app/home/interfaces/ContactDetails.ts
@@ -0,0 +1,10 @@
+export interface ContactDetails{
+    firstName: string;
+    middleName?: string;
+    lastName?: string;
+    phone?: string;
+    email?: string;
+    profile?: string;
+    address?: string;
+    note?: string;
+}
